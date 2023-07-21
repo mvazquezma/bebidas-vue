77d1547222b967167ca109ab8d71cf20ae3e5670
mvazquezma <mvm51090@gmail.com>
2023-07-21 17:18:11 +0200

    Detect index page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <script setup>
-    import { RouterLink } from 'vue-router';
+     import { computed } from 'vue'
+    import { RouterLink, useRoute } from 'vue-router';
     import NavLink from './UI/NavLink.vue';
+
+    const route = useRoute()
+
+    const paginaInicio = computed(() => route.name === 'inicio')
+
+    console.log(route);
 </script>
 
 <template>
     <header
         class="bg-slate-800"
+        :class="{header: paginaInicio}"
     >
         <div class="mx-auto container px-5 py-16">
             <div class="flex justify-between items-center">
@@ -30,6 +38,7 @@
 
             <form
                 class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+                v-if="paginaInicio"
             >
                 <div class="space-y-4">
                     <label 
@@ -66,4 +75,9 @@
 </template>
 
 <style scoped>
+.header {
+    background-image: url('/img/bg.jpg');
+    background-size: cover;
+    background-position: center;
+}
 </style>
