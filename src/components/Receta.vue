d7a056fc9c0027a72de18f0aa037689acc86283b
mvazquezma <mvm51090@gmail.com>
2023-07-24 17:07:14 +0200

    style on receta card

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -7,7 +7,22 @@
 </script>
 
 <template>
-    <div>
-        <h2>{{ receta.strDrink }}</h2>
+    <div class="border shadow-lg">
+        <div class="overflow-hidden">
+            <img 
+                :src="receta.strDrinkThumb"
+                class="hover:scale-125 transition-transform hover:rotate-2"
+                :alt="'Imagen de ' + receta.strDrink"
+            />
+        </div>
+        <div class="p-5">
+            <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+            <button
+                type="button"
+                class="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white"
+            >
+                Ver receta
+            </button>
+        </div>
     </div>
 </template>
